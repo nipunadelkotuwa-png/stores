cd15b961c60526c88ce18528251e473cda390a1f
Harsha Lakshan Jayathissa <harshajayathissa99@gmail.com>
2026-08-11 16:00:32 +0530

    chore: add netlify vite plugin for react router ssr

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
+import netlifyPlugin from "@netlify/vite-plugin-react-router";
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter()],
+  plugins: [tailwindcss(), reactRouter(), netlifyPlugin()],
   resolve: {
     tsconfigPaths: true,
   },
